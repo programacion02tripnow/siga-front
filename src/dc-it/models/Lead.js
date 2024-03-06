37d4c0ed6419c8d88297298b02a86b4f3cf69b99
Adrian Bautista <adrianbautistaorozco@gmail.com>
2024-03-05 23:27:32 -0600

    :sparkles: Add executive to leads and badge with custom status color

--- src/dc-it/models/Lead.js
+++ src/dc-it/models/Lead.js
@@ -12,21 +12,38 @@ export default class Lead extends Model {
   endpoint = 'leads';
 
   related = ['lead_channel', 'lead_status', 'user', 'lead_comments']
-
+  actions = [
+    {
+      class: 'btn-secondary',
+      disabled() {
+        return false
+      },
+      text: 'Comisiones',
+      icon: 'SlidersIcon',
+      action: 'commissionSettings',
+      params(model) {
+        return {
+          id: model.getAttr('id'),
+        }
+      },
+    },
+  ]
   fields = [
     new Field('id', 'Código').disabled().noFilterable(),
     new Field('first_name', 'Nombre'),
     new Field('last_name', 'Apellido(s)'),
+    new Relation('user', 'Ejecutivo', User, 'username').applyMask((_val, model) => `${model.getAttr('first_name')} ${model.getAttr('last_name')}`),
     new Field('email', 'Correo'),
     new Field('phone', 'Teléfono', 'number'),
     new BooleanField('is_agency', 'Agencia').hide(),
-    new BooleanField('is_mini_vacs', 'Mini vacs'),
+    new BooleanField('is_mini_vacs', 'Mini vacs').hide(),
     new Relation('lead_channel', 'Canal de contacto', LeadChannel, 'name'),
     new Field('campaign', 'Campaña'),
     new Field('destination', 'Destino').hide(),
     new Field('desirable_date', 'Fecha deseada de viaje', 'date').hide(),
-    new Relation('lead_status', 'Estado', LeadStatus, 'name'),
-    new Relation('user', 'Usuario', User, 'username').hide(),
+    new Relation('lead_status', 'Estado', LeadStatus, 'name').applyMask((_val, model) => {
+      return '<span class="badge badge-pill" style="color: #000;background: ' + model.getAttr('color') + '">' + _val + '</span>'
+    }),
     new MultipleRelation('lead_comments', 'Comentarios', LeadComment, 'comment').hide().noFilterable(),
   ];
 
